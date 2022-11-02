9356151efa4f68b01eab22e394d5306f547d57c5
bnagasahitya <2022MT12310@wilp.bits-pilani.ac.in>
2022-11-02 15:38:07 +0530

    Courses, Registration, Faculty retrieval and integration

---
+++ src/app/registration/registration.component.ts
@@ -0,0 +1,70 @@
+import { RegistrationService } from './registration.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+export class Registration{
+  constructor(public id:number,public registrationname: string,public registrationdate:Date, public content:string, public student_Id: number){
+
+  }
+}
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+
+export class RegistrationComponent implements OnInit {
+
+
+  registration :Registration[] | undefined;
+  message: string | undefined;
+   
+  // new Courses(1,'Maths','xyz'),
+  // new Courses(2,'abc','xyz'),
+  // new Courses(3,'abc','xyz'),
+
+
+  constructor(private registrationService: RegistrationService,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.retrieveData()
+  }
+  retrieveData(){
+    this.registrationService.retrieveRegistrations().subscribe(
+      response => {
+        console.log(response);
+      this.registration = response;
+      }
+    )
+  }
+  deleteRegistration(id: any, registration: Registration){
+    this.registrationService.deleteRegistration(id, registration).subscribe (
+      response => {
+        console.log(response);
+        this.message = `Delete of Course ${id} Successful!`;
+        this.retrieveData();
+      }
+    )
+  }
+  updateRegistration(id: any){
+    console.log(`update ${id}`)
+    this.router.navigate(['addregistration',id])
+    // this.courseService.updateCourse(id).subscribe (
+    //   response => {
+    //     console.log(response);
+    //     this.message = `Delete of Course ${id} Successful!`;
+    //     this.retrieveData();
+    //   }
+    // )
+  
+     
+    
+  }
+  addRegistration() {
+    this.router.navigate(['addregistration',-1])
+  }
+  }
+
+ 
+
+
---
+++ src/app/registration/registration.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { API_URL } from '../app.constants';
+import { Registration } from './registration.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegistrationService {
+  constructor(private http: HttpClient) { }
+
+  retrieveRegistrations(){
+
+    return this.http.get<Registration[]>(`${API_URL}/registrations`);
+    // localhost:8080/course/1
+  }
+  deleteRegistration(id: any, registration:Registration){
+    return this.http.put(`${API_URL}/delete/registration/${id}`,null);
+  }
+  updateRegistration(id: any, registration: Registration){
+    return this.http.put(`${API_URL}/registration/${id}`,registration);
+  }
+  retrieveRegistration(id: any){
+    return this.http.get<Registration>(`${API_URL}/registration/${id}`);
+  }
+  createRegistration(registration: Registration){
+    return this.http.post(
+              `${API_URL}/registration`
+                , registration);
+  }
+
+}
